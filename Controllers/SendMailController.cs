3dcc414416c973d5b9e4485e2ad4e1d5553e2592
Khanh Minh Dao <minhdao933@gmail.com>
2021-07-23 17:24:16 +0700

    commit

--- Controllers/SendMailController.cs
+++ Controllers/SendMailController.cs
@@ -7,47 +7,23 @@ using System.Net.Mail;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class SendMailController : Controller
     {
-        
+
         // GET: SendMail
         public ActionResult Contact()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult Sendmail(Web.Models.MailModel objMail)
+        public ActionResult Contact(MailModel mail)
         {
-            if(ModelState.IsValid)
-            {
-                string from = "minhdao933@gmail.com";
-                using(MailMessage mail = new MailMessage(from,objMail.to))
-                {
-                    mail.Subject = objMail.subject;
-                    mail.Body = objMail.body;
-                    mail.From = new MailAddress(objMail.from);
-                    mail.IsBodyHtml = false;
-                    using(SmtpClient smtp = new SmtpClient())
-                    {
-                        smtp.Host = "smtp.gmail.com";
-                        smtp.EnableSsl = true;
-                        NetworkCredential networkCredentail = new NetworkCredential(from, "minhtue139");
-                        smtp.UseDefaultCredentials = true;
-                        smtp.Credentials = networkCredentail;
-                        smtp.Port = 587;
-                        smtp.Send(mail);
-                        ViewBag.Message = "Sent";
-                    }
-                    return View("Contact", objMail);
-                }    
-            }
-            else
-            {
-                return View();
-            }
+            mail.SendMail();
+            return View();
         }
     }
 }
